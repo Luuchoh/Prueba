bebb0cad7d9c68cad94b6f7d5ea68ac744a37c10
Luuchoh <luxo997@gmail.com>
2021-06-09 17:13:04 -0500

    Realizando maquetacion

--- prueba-tecnica/src/app/app-routing.module.ts
+++ prueba-tecnica/src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateUserComponent } from './components/create-user/create-user.component';
+import { DetailUserComponent } from './components/detail-user/detail-user.component';
+import { UserListsComponent } from './components/user-lists/user-lists.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'', redirectTo: 'list-user', pathMatch: 'full'},
+  { path:'list-user', component: UserListsComponent},
+  { path: 'create-user', component: CreateUserComponent},
+  { path: 'detail-user/:id', component: DetailUserComponent},
+  { path:'**', redirectTo: 'list-user', pathMatch: 'full'}
 
+]
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- prueba-tecnica/src/app/app.module.ts
+++ prueba-tecnica/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { UserListsComponent } from './components/user-lists/user-lists.component';
+import { CreateUserComponent } from './components/create-user/create-user.component';
+import { DetailUserComponent } from './components/detail-user/detail-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    UserListsComponent,
+    CreateUserComponent,
+    DetailUserComponent
   ],
   imports: [
     BrowserModule,
